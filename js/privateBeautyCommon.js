ff4ac7723b4b2bcdab50d807a32e782ffc1d7456
loubth <loubth@qq.com>
2019-11-07 14:35:33 +0800

    图片懒加载

--- js/privateBeautyCommon.js
+++ js/privateBeautyCommon.js
@@ -3,18 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
     var images = document.querySelectorAll(".imageList img");
     for (var i = 0; i < images.length; i++) {
         images[i].setAttribute("data-src", images[i].src);
-        images[i].setAttribute("src", "https://iph.href.lu/100x200?text=加载中...&bg=DDDDDD");
-        if (document.documentElement.scrollTop + images[i].offsetHeight * 3 > images[i].offsetTop) {
-            images[i].setAttribute("src", images[i].getAttribute("data-src"));
+        if (i > 3) {
+            images[i].setAttribute("src", "https://iph.href.lu/100x200?text=加载中...&bg=DDDDDD");
+        }
+    }
+    window.onscroll = function (e) {
+        var images = document.querySelectorAll(".imageList img");
+        for (var i = 0; i < images.length; i++) {
+            if (document.documentElement.scrollTop + images[i].offsetHeight * 3 > images[i].offsetTop) {
+                images[i].setAttribute("src", images[i].getAttribute("data-src"));
+            }
         }
     }
 });
-window.onscroll = function (e) {
-    var images = document.querySelectorAll(".imageList img");
-    for (var i = 0; i < images.length; i++) {
-        if (document.documentElement.scrollTop + images[i].offsetHeight * 3 > images[i].offsetTop) {
-            images[i].setAttribute("src", images[i].getAttribute("data-src"));
-        }
-    }
-}
+
 
