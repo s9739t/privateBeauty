4c33b427d058e43bfe929ba2abad5aef00c5bedf
loubth <loubth@qq.com>
2019-11-07 17:31:10 +0800

    更改js

--- js/privateBeautyCommon.js
+++ js/privateBeautyCommon.js
@@ -1,17 +1,19 @@
 //图片懒加载
 document.addEventListener('DOMContentLoaded', function () {
     var images = document.querySelectorAll(".imageList img");
+    var placeholderImagePath = "/privateBeauty/image/common/placeholderImage.jpg";
     for (var i = 0; i < images.length; i++) {
         images[i].setAttribute("data-src", images[i].src);
         if (i > 3) {
-            images[i].setAttribute("src", "/privateBeauty/image/common/placeholderImage.jpg");
+            images[i].setAttribute("src", placeholderImagePath);
         }
     }
     window.onscroll = function (e) {
-        var images = document.querySelectorAll(".imageList img");
         for (var i = 0; i < images.length; i++) {
-            if (document.documentElement.scrollTop + images[i].offsetHeight * 3 > images[i].offsetTop) {
-                images[i].setAttribute("src", images[i].getAttribute("data-src"));
+            if (images[i].src.toString() != images[i].getAttribute("data-src").toString()) {
+                if (document.documentElement.scrollTop + images[i].offsetHeight * 3 > images[i].offsetTop) {
+                    images[i].setAttribute("src", images[i].getAttribute("data-src"));
+                }
             }
         }
     }
